1d3cc0d5cfb7d0b393a5a8f0f5627392ba7f1692
Juan Jose <ramirez.orellana.juanjose@gmail.com>
2020-09-20 12:04:09 -0600

    Add DBContext

---
+++ JobBoardApp/JobBoardApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JobBoardApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+        public DbSet<Models.JobBoard> BoardGames { get; set; }
+    }
+}
